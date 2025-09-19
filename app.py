08ab712f736713dbb996e6d81a4d64c5e49859f7
Gavan Sarrafian <gsarrafi55@gmail.com>
2025-09-19 16:04:48 -0500

    Updated index.html

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
-
+@app.route("/")
+def index():
+    # You can pass data into the page if you want:
+    return render_template("index.html", title="Pneumatics Control")
 
 if __name__ == '__main__':
     app.run()
