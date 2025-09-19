b1f57e59681f9b120c802cf0e3a214b14207319d
Gavan Sarrafian <gsarrafi55@gmail.com>
2025-09-19 16:11:57 -0500

    Broke apart css from the original html file

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
 @app.route("/")
 def index():
     # You can pass data into the page if you want:
